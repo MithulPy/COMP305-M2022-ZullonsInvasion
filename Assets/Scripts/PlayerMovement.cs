cda28c65fa2386ddb437d9bcf765f88bb9f45f3c
Miles136 <nemanuel.montoya@gmail.com>
2022-06-10 10:26:50 -0400

    Fixing the Double Jump Problem
    PlayerMovement groundCheckRadius must be set to around 1.15f

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float playerSpeed = 5.0f;
-    private float jumpSpeed = 9f;
-    private Boolean grounded = true;
-    private float jumps = 0;
-    private float jumpMax = 1;
+    public float playerSpeed = 5.0f;
+    public float jumpSpeed = 9f;
+    public Boolean grounded = true;
+    public float groundCheckRadius = 1.15f;
+    public float jumps = 0;
+    public float jumpMax = 1;
     private LayerMask groundMask;
 
     private Rigidbody2D playerRB;
@@ -29,6 +30,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        // grounded = Physics2D.OverlapCircle(transform.position, 2.0f, groundMask);
+        if (grounded) am.SetBool("Jumping", false);
+        else am.SetBool("Jumping", true);
+
+        // Walking
         if (Input.GetButton("Horizontal"))
         {
             am.SetBool("Walking",true);
@@ -40,53 +47,27 @@ public class PlayerMovement : MonoBehaviour
         else {
             am.SetBool("Walking",false);
         }
+
+        // Jumping
         if (Input.GetButtonUp("Jump"))
         {
             am.SetBool("Jumping",true);
-            if ( grounded )
+            if ( grounded ) playerRB.AddForce(new Vector2(0, jumpSpeed),ForceMode2D.Impulse);
+        }
+
+        // Grounding
+        RaycastHit2D groundCheck = Physics2D.Raycast(transform.position, Vector2.down, groundCheckRadius, groundMask);
+        if (groundCheck)
+        {
+            if (groundCheck.collider.CompareTag("Ground"))
             {
-                jumps++;
-                Jump();
-            }
-        }
-        Ground();
-    }
-
-    private void MovePlayer()
-    {
-        if (Input.GetButton("Horizontal"))
-        {
-            am.SetBool("Walking",true);
-            float axis = Input.GetAxis("Horizontal");
-            if (axis < 0) sr.flipX = true; else sr.flipX = false;
-            transform.Translate( axis * Vector2.right * playerSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-        else {
-            am.SetBool("Walking",false);
-        }
-    }
-
-    private void Jump()
-    {
-        playerRB.AddForce(new Vector2(0, jumpSpeed),ForceMode2D.Impulse);
-    }
-
-    private void Ground()
-    {
-        var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, groundMask);
-        if (groundCheck.collider.gameObject != null)
-        {
-            Debug.Log("Not Null");
-
-            if (groundCheck.collider.CompareTag("Ground")) {
-                jumps = 0;
                 grounded = true;
                 am.SetBool("Jumping",false);
             }
             else grounded = false;
-        } else grounded = false;
-    }
+        }
+        else grounded = false;
 
+    }
 
 }
